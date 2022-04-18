dfd758a74420514ff98e2430ffc1f6e3e6009bea
DuarteStokes <stattmiller93@gmail.com>
2022-04-18 11:36:14 +0100

    Fixed permission error on linux assuming sudo privileges.

--- reefledge/_filesystem_utils.py
+++ reefledge/_filesystem_utils.py
@@ -1,11 +1,14 @@
+from typing import Final, List, Callable
+import sys
 import shutil
-from typing import List
 import os
 import stat
-import sys
 import subprocess
 import warnings
 from zipfile import ZipFile
+
+ON_LINUX: Final[bool] = sys.platform.startswith('linux')
+ON_WINDOWS: Final[bool] = sys.platform.startswith('win')
 
 
 def remove_directory(directory_name: str) -> None:
@@ -22,16 +25,17 @@ def _remove_directory_platform_specific(directory_name: str) -> None:
     if ' ' in directory_name:
         directory_name = '"' + directory_name + '"'
 
-    if sys.platform.startswith('linux'):
+    shell_command: str
+    if ON_LINUX:
         shell_command = f'rm -rf {directory_name}'
-    elif sys.platform.startswith('win'):
+    elif ON_WINDOWS:
         shell_command = f'rmdir /s /q {directory_name}'
     else:
         raise OSError('Unsupported operating system.')
 
     __remove_directory_via_subprocess(shell_command, directory_name)
 
-def __change_file_permissions(root_directory_name: str) -> None:
+def __change_file_permissions(*, root_directory_name: str) -> None:
     def change_single_file_permissions(file_path: str) -> None:
         try:
             os.chmod(file_path, stat.S_IWUSR)
@@ -48,15 +52,24 @@ def __change_file_permissions(root_directory_name: str) -> None:
 
 def __remove_directory_via_subprocess(
     shell_command: str, directory_name: str) -> None:
-    ####################################################################
+    ###########################################################################
+    is_sudo_command: Callable[[str], bool] = \
+        lambda command: command.startswith('sudo')
+
     shell_command_split: List[str] = shell_command.split(' ')
     completed_process = subprocess.run(shell_command_split)
 
     if completed_process.returncode != 0:
-        warnings.warn(f'''
-            Failed to remove the "{directory_name}" directory.
-            Please remove it manually, if possible.
-        ''')
+        if ON_LINUX and (not is_sudo_command(shell_command)):
+            __remove_directory_via_subprocess(
+                shell_command=('sudo ' + shell_command),
+                directory_name=directory_name
+            )
+        else:
+            warnings.warn(f'''
+                Failed to remove the "{directory_name}" directory.
+                Please remove it manually, if possible.
+            ''')
 
 
 def extract_zip_file(zip_file_name: str) -> None:
