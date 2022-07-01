a91fff9b28b3a38d5ab2f09f14bf1eca6b4349b1
DuarteStokes <stattmiller93@gmail.com>
2022-07-01 13:12:53 +0100

    wip

--- reefledge/version.py
+++ reefledge/version.py
@@ -6,5 +6,5 @@ import pdoc
 
 __pdoc__: Final[Dict[str, bool]] = {'__version__': True}
 
-__version__: Final[str] = '1.4.0' + '-alpha.2'
+__version__: Final[str] = '1.4.0' + '-beta.0' # Do NOT use double quotes!
 """Package version number"""
