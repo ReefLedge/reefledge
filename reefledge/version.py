6480a587147239e8c8b1bcadfe0fe15f01847588
DuarteStokes <stattmiller93@gmail.com>
2022-08-23 18:57:21 +0100

    v1.4.0-beta.6

--- reefledge/version.py
+++ reefledge/version.py
@@ -7,5 +7,5 @@ import pdoc
 __pdoc__: Final[Dict[str, bool]] = {'__version__': True}
 
 # Do NOT use double quotes!
-__version__: Final[str] = '1.4.0' + '-beta.5'
+__version__: Final[str] = '1.4.0' + '-beta.6'
 """Package version number"""
