8e3609745ac260cf1c7e652d36bb95a9437b496a
DuarteStokes <stattmiller93@gmail.com>
2022-10-14 12:03:24 +0100

    Replace HTTP with HTTPS in request for IPv4 address.

--- reefledge/network_optimization/ipv4_address_to_geo_coordinates.py
+++ reefledge/network_optimization/ipv4_address_to_geo_coordinates.py
@@ -1,6 +1,8 @@
 from typing import Tuple, Optional as Opt, Dict, Union, List, cast
+import warnings
 
 import requests
+from requests.exceptions import SSLError
 
 PAIR = Tuple[float, float]
 
@@ -14,11 +16,18 @@ def ipv4_address_to_geo_coordinates(ipv4_address: Opt[str] = None) -> PAIR:
 
 def _get_data_in_json_format(ipv4_address: Opt[str]) -> Dict[str, str]:
     url: str = __assemble_url(ipv4_address)
-    response = requests.get(url)
+
+    try:
+        response = requests.get(url)
+    except SSLError:
+        with warnings.catch_warnings():
+            warnings.simplefilter('ignore')
+            response = requests.get(url, verify=False)
+
     return response.json()
 
 def __assemble_url(ipv4_address: Opt[str]) -> str:
-    url: str = 'http://ipinfo.io/' # Not HTTPS
+    url: str = 'https://ipinfo.io/'
 
     if ipv4_address is not None:
         url += f"{ipv4_address}/"
@@ -28,13 +37,12 @@ def __assemble_url(ipv4_address: Opt[str]) -> str:
 
 
 def _extract_geo_coordinates(data: Dict[str, str]) -> PAIR:
-    geo_coordinates: Union[List[str], PAIR]
-    geo_coordinates = data['loc'].split(',') # list[str]
-    assert len(geo_coordinates) == 2
+    _geo_coordinates: List[str] = data['loc'].split(',')
+    assert len(_geo_coordinates) == 2
 
     geo_coordinates = cast(
         PAIR,
-        tuple([float(c) for c in geo_coordinates])
+        tuple([float(c) for c in _geo_coordinates])
     )
 
     return geo_coordinates
